c7bfa186d9a36b13ba707d037861cdbd2a06948b
AntonioFarracho <amgfarracho@gmail.com>
2015-11-21 17:52:48 +0000

    Update 21/11 17:45 - List ADT

--- park_config.h
+++ park_config.h
@@ -18,3 +18,13 @@
  void AlocaMatrizPark(Park * p);
 
  Park *NewPark(int columns, int lines, int entrances, int nr_accesses, int floors);
+
+ int Char_to_Number (char * c);
+
+ void Map_to_matrix (Park * p, FILE * f, int line, int column, int _floor);
+
+ void Read_Doors_info (Park * p, FILE * f, int *i, int *j);
+
+ void Read_floor (Park * p, FILE * f, int _floor, int *i, int *j);
+
+ Park *ReadFilePark (char * file);
