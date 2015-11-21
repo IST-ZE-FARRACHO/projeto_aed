30d36b9b9dc45fade5fde5ab8b96aafbb232e01b
José Correia <menesesjmmc@gmail.com>
2015-11-21 17:48:26 +0000

    Updated

---
+++ Item.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *
+ * File Name: Item.h
+ * Author:    José Correia / António Farracho
+ *
+ * NAME
+ *     Item.h - definition of item
+ *
+ * SYNOPSIS
+ *     #include <stdlib.h>
+ *     #include <stdio.h>
+ *     #include <string.h>
+ *
+ * DESCRIPTION
+ *
+ * DIAGNOSTICS
+ *
+ *****************************************************************************/
+
+typedef void *Item;
